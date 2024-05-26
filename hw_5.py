2350f7b006272d81a4caf11a2617d94181872bb3
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-26 18:17:28 +0300

    update_2

--- hw_5.py
+++ hw_5.py
@@ -2,7 +2,10 @@ import aiohttp
 import asyncio
 import platform
 
-urls = ['https://www.google.com', 'https://www.python.org/asdf', 'https://duckduckgo.com', 'http://test', 'asdf']
+
+
+class HttpError(Exception):
+    pass
 
 
 async def request(url):
@@ -10,19 +13,25 @@ async def request(url):
         try:
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    html = await resp.text()
-                    print(url, html[:150])
+                    result = await resp.json()
+                    return result
                 else:
-                    print(f"Error status: {resp.status} for {url}")
+                    raise HttpError(f"Error status: {resp.status} for {url}")
         except (aiohttp.ClientConnectorError, aiohttp.InvalidURL) as err:
-            print(f'Connection error: {url}', str(err))
+            raise HttpError(f'Connection error: {url}', str(err))
 
 
 async def main():
-    pass
+    try:
+        response = await request('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
+        return response
+    except HttpError as err:
+        print(err)
+        return None
 
 
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+    r = asyncio.run(main())
+    print(r)
